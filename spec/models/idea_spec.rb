972d5e27143ae83b225487b25c977ca2d76d596e
Dawid Dylowicz <ddylowicz@gmail.com>
2016-04-28 20:28:28 +0200

    Implement tests for Idea model validation

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -4,4 +4,17 @@ describe Idea do
   it "has a valid factory" do
     expect(FactoryGirl.create(:idea)).to be_valid
   end
+
+  it "is invalid without a title" do
+    expect(FactoryGirl.build(:idea, title: nil)).not_to be_valid
+  end
+
+  it "is invalid without status" do
+    expect(FactoryGirl.build(:idea, status: nil)).not_to be_valid
+  end
+
+  it "is invalid without score" do
+    expect(FactoryGirl.build(:idea, score: nil)).not_to be_valid
+    pry
+  end
 end
