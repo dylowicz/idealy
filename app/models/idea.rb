1133f8027d2e0a0c62e1a464bb7b636273cdaee3
Dawid Dylowicz <ddylowicz@gmail.com>
2016-04-28 20:28:04 +0200

    Add validation for status and score in Idea model

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,4 +1,6 @@
 class Idea < ApplicationRecord
   validates :title, presence: true
+  validates :status, presence: true
+  validates :score, presence: true
   enum status: [:new, :active, :dropped, :done], _suffix: true
 end
