f08e080157fe0076b28a28802c455ca0bc3bfb62
Dawid Dylowicz <ddylowicz@gmail.com>
2016-05-24 04:33:46 +0200

    Add CSS to Task delete icon on hover

--- app/assets/javascripts/components/tasks/Task.es6.jsx
+++ app/assets/javascripts/components/tasks/Task.es6.jsx
@@ -1,11 +1,11 @@
 const Task = ({onTaskClick, onTaskDeleteClick, completed, title}) => (
     <li className="list-group-item-unstyled">
-        <div className="checkbox" onClick={onTaskClick}>
-            <label style={ {textDecoration: completed ? 'line-through' : 'none'} }>
+        <div className="checkbox" id="task">
+            <label onClick={onTaskClick} style={ {textDecoration: completed ? 'line-through' : 'none'} }>
                 <input type="checkbox" value="" checked={completed} readOnly/>
                 {' '}{title}
             </label>
+            <span onClick={onTaskDeleteClick} className="glyphicon glyphicon-trash pull-right" id="delete-task"></span>
         </div>
-        {' '}<span onClick={onTaskDeleteClick} className="glyphicon glyphicon-trash pull-right"></span>
     </li>
 );
