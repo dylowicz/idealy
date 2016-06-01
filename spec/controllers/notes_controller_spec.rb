5e283230703aa4b379713a83c52724c9a7c9dabd
Dawid Dylowicz <ddylowicz@gmail.com>
2016-06-01 22:56:28 +0200

    Finish Notes contorller specs with unauthorized validations

--- spec/controllers/notes_controller_spec.rb
+++ spec/controllers/notes_controller_spec.rb
@@ -1,71 +1,121 @@
 require 'spec_helper'
 
+UNAUTHORIZED_ERROR = { error: "Access denied" }
+
 describe NotesController,  type: :controller do
   before(:all) do
-    @authorized_user = FactoryGirl.create(:user)
-    @unauthorized_user = FactoryGirl.create(:user)
+    @user = FactoryGirl.create(:user)
   end
 
   describe "GET #index" do
     before(:all) do
-      @idea = FactoryGirl.create(:idea, user: @authorized_user)
+      @idea = FactoryGirl.create(:idea, user: @user)
       @notes = FactoryGirl.create_list(:note, 3, idea_id: @idea.id)
     end
 
-    before(:each) do
-      session[:user_id] = @authorized_user.id
-      get :index, params: { idea_id: @idea.id }, xhr: true
+    context "as authorized user" do
+      before(:each) do
+        session[:user_id] = @user.id
+        get :index, params: { idea_id: @idea.id }, xhr: true
+      end
+
+      it { is_expected.to respond_with :ok }
+      it { is_expected.to respond_with_content_type :json }
+      it { expect(response.body).to be_json_eql(@notes.to_json).excluding('idea_id') }
     end
 
-    it { is_expected.to respond_with :ok }
-    it { is_expected.to respond_with_content_type :json }
-    it { expect(response.body).to be_json_eql(@notes.to_json).excluding('idea_id') }
+    context "as unauthorized user" do
+      before(:each) do
+        get :index, params: { idea_id: @idea.id }, xhr: true, format: :json
+      end
+
+      it { is_expected.to respond_with :unauthorized }
+      it { is_expected.to respond_with_content_type :json }
+      it { expect(response.body).to be_json_eql(UNAUTHORIZED_ERROR.to_json) }
+    end
   end
+
 
   describe "POST #create" do
     before(:all) do
-      @idea = FactoryGirl.create(:idea, user: @authorized_user)
-    end
-
-    before(:each) do
-      session[:user_id] = @authorized_user.id
+      @idea = FactoryGirl.create(:idea, user: @user)
       @note_attributes = FactoryGirl.attributes_for(:note, idea: @idea)
-      post :create, params: { idea_id: @idea.id, note: @note_attributes }, xhr: true
     end
 
-    it { is_expected.to respond_with :created }
-    it { is_expected.to respond_with_content_type :json }
-    it { expect(response.body).to be_json_eql @note_attributes.to_json }
+    context "as authorized user" do
+      before(:each) do
+        session[:user_id] = @user.id
+        post :create, params: { idea_id: @idea.id, note: @note_attributes }, xhr: true
+      end
+
+      it { is_expected.to respond_with :created }
+      it { is_expected.to respond_with_content_type :json }
+      it { expect(response.body).to be_json_eql @note_attributes.to_json }
+    end
+
+    context "as unauthorized user" do
+      before(:each) do
+        post :create, params: { idea_id: @idea.id, note: @note_attributes }, xhr: true, format: :json
+      end
+
+      it { is_expected.to respond_with :unauthorized }
+      it { is_expected.to respond_with_content_type :json }
+      it { expect(response.body).to be_json_eql(UNAUTHORIZED_ERROR.to_json) }
+    end
   end
 
   describe "PATCH #update" do
     before(:all) do
-      @idea = FactoryGirl.create(:idea, user: @authorized_user)
+      @idea = FactoryGirl.create(:idea, user: @user)
       @note = FactoryGirl.create(:note, idea: @idea)
-    end
-
-    before(:each) do
-      session[:user_id] = @authorized_user.id
       @note_attributes = FactoryGirl.attributes_for(:note, idea: @idea)
-      patch :update, params: { id: @note.id, idea_id: @idea.id, note: @note_attributes }, xhr: true
     end
 
-    it { is_expected.to respond_with :ok }
-    it { is_expected.to respond_with_content_type :json }
-    it { expect(response.body).to be_json_eql @note_attributes.to_json }
+    context "as authorized user" do
+      before(:each) do
+        session[:user_id] = @user.id
+        patch :update, params: { id: @note.id, idea_id: @idea.id, note: @note_attributes }, xhr: true
+      end
+
+      it { is_expected.to respond_with :ok }
+      it { is_expected.to respond_with_content_type :json }
+      it { expect(response.body).to be_json_eql @note_attributes.to_json }
+    end
+
+    context "as unauthorized user" do
+      before(:each) do
+        patch :update, params: { id: @note.id, idea_id: @idea.id, note: @note_attributes }, xhr: true, format: :json
+      end
+
+      it { is_expected.to respond_with :unauthorized }
+      it { is_expected.to respond_with_content_type :json }
+      it { expect(response.body).to be_json_eql(UNAUTHORIZED_ERROR.to_json) }
+    end
   end
 
   describe "DELETE #destroy" do
     before(:all) do
-      @idea = FactoryGirl.create(:idea, user: @authorized_user)
+      @idea = FactoryGirl.create(:idea, user: @user)
       @note = FactoryGirl.create(:note, idea: @idea)
     end
 
-    before(:each) do
-      session[:user_id] = @authorized_user.id
-      delete :destroy, params: { id: @note.id, idea_id: @idea.id }, xhr: true
+    context "as authorized user" do
+      before(:each) do
+        session[:user_id] = @user.id
+        delete :destroy, params: { id: @note.id, idea_id: @idea.id }, xhr: true
+      end
+
+      it { is_expected.to respond_with :no_content }
     end
 
-    it { is_expected.to respond_with :no_content }
+    context "as unauthorized user" do
+      before(:each) do
+        delete :destroy, params: { id: @note.id, idea_id: @idea.id }, xhr: true, format: :json
+      end
+
+      it { is_expected.to respond_with :unauthorized }
+      it { is_expected.to respond_with_content_type :json }
+      it { expect(response.body).to be_json_eql(UNAUTHORIZED_ERROR.to_json) }
+    end
   end
 end
