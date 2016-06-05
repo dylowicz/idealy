77e35e2ab09109a2fe94c059a1fa5479fc6c7478
Dawid Dylowicz <ddylowicz@gmail.com>
2016-06-05 14:31:19 +0200

    Setup React dependencies with Browserify and Reactify

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,7 @@ module Idealy
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.browserify_rails.commandline_options = "--transform reactify --extensions .jsx"
-    # config.browserify_rails.commandline_options = "-t [ reactify --presets [ es2015 react ] --extensions .es6.jsx ]"
-    # config.browserify_rails.commandline_options = "-t [ babelify --presets [ es2015 react ] --extension=\".es6.jsx\" ]"
+    # config.browserify_rails.commandline_options = "-t [ babelify --presets [ es2015 react ] ]"
     config.browserify_rails.commandline_options = "-t reactify --extension=\".es6.jsx\""
   end
 end
