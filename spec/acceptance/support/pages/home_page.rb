7437298defd479a8685d6ce0f5cbd75eda3f6a6f
Dawid Dylowicz <ddylowicz@gmail.com>
2016-06-25 01:48:59 +0200

    Add Log In link to sign up form

--- spec/acceptance/support/pages/home_page.rb
+++ spec/acceptance/support/pages/home_page.rb
@@ -1,3 +1,9 @@
 class HomePage < BasePage
   page_url 'http://localhost:3000/'
+
+  link(:signup, id: 'sign-up-link')
+
+  def go_to_signup
+    signup
+  end
 end
--- spec/acceptance/users_spec.rb
+++ spec/acceptance/users_spec.rb
@@ -9,19 +9,17 @@ describe "Users" do
   end
 
   describe "Sign up" do
-    it "goes to login page" do
-      on(Dashboard).go_to_login
-      expect(on(LoginPage).login_header_element.when_visible.text).to eq "Log in"
+    it "finds sign-up link on home page" do
+      expect(on(HomePage).signup_element.when_visible.text).to eq "Sign up now!"
     end
 
     it "goes to sign up page" do
-      on(LoginPage).go_to_signup
+      on(HomePage).go_to_signup
       expect(on(SignUpPage).signup_header_element.when_visible.text).to eq "Sign up"
     end
 
     it "creates a user" do
       on(SignUpPage).signup(@name, @email, @password)
-      expect(on(IdeasPage).flash_message_element.when_visible.text).to eq "User #{@name} successfully created!"
     end
 
     it "automatically logs in a user" do
@@ -31,11 +29,20 @@ describe "Users" do
     it "redirects to Ideas page" do
       expect(on(IdeasPage).ideas_header_element.visible?).to be_truthy
     end
+
+    it "displays information about successful user creation" do
+      expect(on(IdeasPage).flash_message_element.when_visible.text).to eq "User #{@name} successfully created!"
+    end
   end
 
   describe "Logout" do
     it "logouts a user" do
       on(Dashboard).logout_user
+      expect(on(Dashboard).user_name_element.exists?).to be_falsy
+      expect(on(Dashboard).login_element.when_visible.text).to eq "Sign in"
+    end
+
+    it "displays information about successful logout" do
       expect(on(HomePage).flash_message_element.when_visible.text).to eq "Successfully logged out."
     end
 
@@ -56,12 +63,15 @@ describe "Users" do
 
     it "logins the user" do
       on(LoginPage).login_with(@email, @password)
-      expect(on(IdeasPage).flash_message_element.when_visible.text).to eq "Successfully logged in."
       expect(on(Dashboard).user_name_element.when_visible.text).to eq @name
     end
 
     it "redirects to Ideas page" do
       expect(on(IdeasPage).ideas_header_element.visible?).to be_truthy
+    end
+
+    it "displays information about successful login" do
+      expect(on(IdeasPage).flash_message_element.when_visible.text).to eq "Successfully logged in."
     end
   end
 end
