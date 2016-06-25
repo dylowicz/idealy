6cd892abf86df85bbec9c1dee9fba70fc750c285
Dawid Dylowicz <ddylowicz@gmail.com>
2016-06-25 18:00:26 +0200

    Add User acceptance tests for scenarios with invalid data

--- spec/acceptance/support/pages/signup_page.rb
+++ spec/acceptance/support/pages/signup_page.rb
@@ -2,6 +2,7 @@ class SignUpPage < BasePage
   page_url 'http://localhost:3000/signup'
 
   h1(:signup_header, text: "Sign up")
+  unordered_list(:errors, id: 'errors-list')
   text_field(:name_field, id: 'name-textfield')
   text_field(:email_field, id: 'email-textfield')
   text_field(:password_field, id: 'password-textfield')
@@ -14,5 +15,9 @@ class SignUpPage < BasePage
     self.password_field = password
     self.password_confirmation_field = password
     create_account
+  end
+
+  def find_error(error)
+    errors_element.list_item_elements.find { |element| element.text == error }
   end
 end
